ded2a319182221360f7aee33da08049913806505
TY2810 <yolfry@gmail.com>
2024-08-26 11:57:16 -0400

    Update deployment script for Rootstock Testnet

--- contracts/script/Counter.s.sol
+++ contracts/script/Counter.s.sol
@@ -10,10 +10,12 @@ contract CounterScript is Script {
     function setUp() public {}
 
     function run() public {
-        vm.startBroadcast();
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
 
         counter = new Counter();
 
         vm.stopBroadcast();
     }
 }
+
